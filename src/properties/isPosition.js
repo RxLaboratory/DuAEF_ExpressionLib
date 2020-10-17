06caea8660f8b61d6d3cccdba43974106fe1704e
Duduf <git@duduf.com>
2020-10-17 19:42:33 +0200

    Update isPosition.js

--- src/properties/isPosition.js
+++ src/properties/isPosition.js
@@ -6,10 +6,5 @@
  * @return {boolean} true if the property is the transform.position property.
  */
 function isPosition(prop) {
-	if (!(prop.value instanceof Array)) return false;
-	if (prop.value.length > 3) return false;
-	//compare the name, index and value with the real position
-	if ( prop === transform.position ) return true;
-	if ( prop === position ) return true;
-	return false;
+	return  prop === transform.position;
 }
